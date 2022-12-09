3a3b4e80dea46d29da3109eecf3f1f05bd0f228a
Momin Khan <momin@teamflowhq.com>
2022-12-09 17:30:29 -0500

    organization

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Holy War
 QR multiplayer game
+
+The entry point is `scan.html?loc=location-id`, which will then redirect you around as necessary
--- scan.js
+++ scan.js
@@ -1,6 +1,3 @@
-const DESC_PROMPT = `Write an interesting name for a location that would exist at some point in the history of real world Earth, that ties together the spirit and meaning of the following phrases which are separated by newlines. Print the title by itself in a new line, using capitalized words. Then write a 2-sentence description of maximum 700 characters of the above location using flowery prose, which incorporates the key nouns in the phrases repeatedly and with great detail. Print the description by itself after another new line.\n\n`;
-const IMAGE_PROMPT = `An impressive award-winning landscape that matches the following description, high definition with lots of lines and bold colors, in the style of famous illustrators, trending on art station: `;
-
 void init();
 
 async function init() {
@@ -28,35 +25,8 @@ async function init() {
 
     const place = (await db.collection('places').doc(loc).get()).data();
     if (!place) {
-        // get thematic phrases
-        const p1 = prompt('Phrase 1');
-        const p2 = prompt('Phrase 2');
-        const p3 = prompt('Phrase 3');
-        const p = `${p1}. \n${p2}. \n${p3}. `;
-
-        //  generate title, description
-        const openaiKey = (
-            await db.collection('config').doc('openai_key').get()
-        ).data().value;
-        const completion = await getCompletion(
-            openaiKey,
-            `${DESC_PROMPT} ${p}`
-        );
-        const [title, desc] = completion.split('\n\n');
-
-        //  generate image
-        const imageUrl = await getImage(openaiKey, `${IMAGE_PROMPT} ${p}`);
-
-        //  save to firebase with location key
-        await db.collection('places').doc(loc).set({
-            title,
-            desc,
-            imageUrl,
-            prompt: p,
-        });
-
-        // reload to rescan with the new location
-        location.reload();
+        location.pathname = '/discover.html';
+        return;
     }
 
     //  "you found: Location X"
@@ -66,37 +36,4 @@ async function init() {
     //  influence sect
     //      increment points for all users in sect
     //  sect X is winning, do you want to convert?
-}
-
-async function getCompletion(key, prompt) {
-    const data = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${key}`,
-        },
-        body: JSON.stringify({
-            model: 'text-davinci-003',
-            prompt,
-            temperature: 0.7,
-            max_tokens: 256,
-        }),
-    }).then((res) => res.json());
-    return data.choices[0].text.trim();
-}
-
-async function getImage(key, prompt) {
-    const data = await fetch('https://api.openai.com/v1/images/generations', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${key}`,
-        },
-        body: JSON.stringify({
-            prompt,
-            size: '512x512',
-            n: 1,
-        }),
-    }).then((res) => res.json());
-    return data.data[0].url;
 }
